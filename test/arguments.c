02e759897d97974addb2ec270854738337ae03fa
Gwinchy <gmunyui254@gmail.com>
2023-09-11 13:50:25 -0700

    arguments debugging

--- test/arguments.c
+++ test/arguments.c
@@ -1,7 +1,45 @@
 #include "shell.h"
 
-int main(ac, **argv)
+/**
+ * main - Starting point
+ *
+ * Return: 0 Always successful
+ */
+
+int main(void)
 {
-	arguments()
+	char *command;
+	char *argv[4];
+	int status;
+	pid_t pid;
+
+	command = "/simple_shell/ex_path/my-exe";
+	argv[] = {command, "arg1", "arg2", NULL};
+	if (access(command, X_OK) == -1)
 	{
-		if (input 
+		perror("command not found");
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execvp(command, argv);
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			printf("Child process exited with status %d\n", WEXITSTATUS(status));
+		}
+	}
+	return (0);
+}
+
